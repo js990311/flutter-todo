353ac5f8664cd353db40377ec8bb88c13e2a1016
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-05-07 15:14:14 +0900

    feat: todo_item_widget layout

--- lib/widgets/todo_item_widget.dart
+++ lib/widgets/todo_item_widget.dart
@@ -13,21 +13,41 @@ class TodoItem extends StatelessWidget {
       margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
       elevation: 1, // 그림자 높이
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      child: Column(
-        children: [
-          Row(
-            // 제목부
-            children: [
-              Text(todo.title),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.edit, size: 10),
-              ),
-            ],
-          ),
-          // 본문부
-          Text(todo.content),
-        ],
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              // 제목부
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(flex: 1, child: Text(todo.status.name)),
+                Expanded(
+                  child: Text(
+                    todo.title,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.edit, size: 10),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 4),
+              child: Text(todo.content),
+            ),
+            // 본문부
+          ],
+        ),
       ),
     );
   }
